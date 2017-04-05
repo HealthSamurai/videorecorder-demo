9fee6ad56a86f0edd367e0d2a51871e095f9a2f9
niquola <niquola@gmail.com>
2017-04-05 18:46:57 +0300

    add labels to buttons

--- src/cljs/aidbox_ui/pages/config.cljs
+++ src/cljs/aidbox_ui/pages/config.cljs
@@ -69,8 +69,8 @@
         [:div.buttons
          [:div.timer (:time @state) " sec"]
          (if (= :in-progress phase)
-           [:button.stop  {:title "stop recording" :on-click stop-recording}]
-           [:button.start {:title "start recording" :on-click start-recording}])]]
+           [:button.stop  {:title "stop recording" :on-click stop-recording} "Start Recording"]
+           [:button.start {:title "start recording" :on-click start-recording} "Stop Recording"])]]
 
        [:br]
        [:br]
